580a884f3c75d26fe62aab4f85742839c8e85b56
Violet <cakewithsteak@gmail.com>
2018-04-28 10:07:34 +0200

    Didn't texture the sphere

--- src/main/resources/szakkor6/frag.glsl
+++ src/main/resources/szakkor6/frag.glsl
@@ -3,7 +3,7 @@ uniform sampler2D texture;
 varying vec4 vertTexCoord;
 
 void main() {
-    //gl_FragColor = texture2D(texture,vertTexCoord.st);
-    //gl_FragColor = vec4(vertTexCoord.rg,1,1);
-    gl_FragColor = vec4(1 - int(gl_FrontFacing),int(gl_FrontFacing),0,1);
+    //gl_FragColor = texture2D(texture,vertTexCoord.st);    //Actually texture
+    //gl_FragColor = vec4(vertTexCoord.rg,1,1);     //Show the uv coords
+    //gl_FragColor = vec4(1 - int(gl_FrontFacing),int(gl_FrontFacing),0,1);     //Show the triengles' facing (green = outside, red = inside)
 }
