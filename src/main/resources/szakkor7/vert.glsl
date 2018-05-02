4ab3bc15ce34cecafcb3aeaaa9c1bc01e4e73719
Vince <cakewithsteak@gmail.com>
2018-05-02 17:44:23 +0200

    Added lighting, walls, the radio, and the semi-functional FPS camera

--- src/main/resources/szakkor7/vert.glsl
+++ src/main/resources/szakkor7/vert.glsl
@@ -1,7 +1,31 @@
-uniform mat4 transform;
+const int numLights = 4;
 
+uniform mat4 transform;
+uniform mat4 texMatrix;
+uniform mat3 normalMatrix;
+uniform vec4 lightPosition[numLights];
+uniform mat4 modelview;
+
+attribute vec4 texCoord;
 attribute vec4 position;
+attribute vec3 normal;
+
+varying vec4 vertTexCoord;
+varying vec3 ecNormal;
+varying vec3 ecViewDir;
+varying vec3 ecLightDir[numLights];
+varying vec3 distance[numLights];
+
 void main() {
      // normalized device coordinates [-1,1]
      gl_Position = transform * position;
+     vertTexCoord = texMatrix * texCoord;
+     ecNormal = normalMatrix * normal;
+     vec4 ecPostiton = (modelview * position);
+     ecViewDir = -(normalize(ecPostiton)).xyz;
+     for(int i = 0;i < numLights;++i) {
+        distance[i] = (lightPosition[i] - ecPostiton).xyz;
+        ecLightDir[i] = normalize(distance[i]);
+     }
+
 }
